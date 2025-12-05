3fdb57e64e99e2ec9034abb968da696dce98d586
Pranav Rawat <prawat@ucdavis.edu>
2025-12-05 10:45:43 -0800

    helper function to get data from each row

--- AgendaImport/AgendaImport/import_agenda.py
+++ AgendaImport/AgendaImport/import_agenda.py
@@ -7,18 +7,31 @@ import os
 error = {}
 #Find error code associated with print message 
 def findErr(errCode):
-    return
+    return error[errCode]
 
 #TODO Clean functions
 def clean_dataframe(df):
     return
+#need some way to fetch data
+def getColData(df, row, columnName):
+    col_idx = next((column for column in df.columns if str(column) == columnName), None)
+
+    if col_idx:
+        value = row[col_idx]
+        return str(value).strip()
+    return ""
+
 
 #TODO Read, 
 def create_table(file):
     database = db()
     database.create_table
 
-    dataframe = pd.read_excel(file, header = 14) #start from column 14
+    dataframe = pd.read_excel(file, header = 14) #start from row 14
+    
+    for idx, row in dataframe.iterrows():
+        colData = {
+        }
     return
 
 #run script - more or less finished
