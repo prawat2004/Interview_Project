efb45116f02617882e8069e5aafd103077b2722d
Pranav Rawat <prawat@ucdavis.edu>
2025-12-05 11:20:30 -0800

    fixed some function call issues, added session/subsession as a type to parse for lookup

--- AgendaImport/AgendaImport/import_agenda.py
+++ AgendaImport/AgendaImport/import_agenda.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 #TODO: Add error handling codes associated with message, maybe change this to a separate file?
-error = {}
+error = {0: "Executed Properly"}
 #Find error code associated with print message 
 
 
@@ -23,33 +23,31 @@ def get_col_data(df, row, columnName):
         return str(value).strip()
     return ""
 
-
-#TODO Read, 
-
 #Data Format for Reference
 # "*Date" -> date
 # "*Time Start" -> time_start
 # "*Time End" -> time_end
-# "*Session or Sub-session(Sub)" -> not held yet
+# "*Session or Sub-session(Sub)" -> type
 # "*Session Title" -> title
 # "*Room/Location" -> location
 # "Description" -> description
 # "Speakers" -> speaker
 def create_table(file):
     database = db()
-    database.create_table
+    database.create_table()
 
     dataframe = pd.read_excel(file, header = 14) #start from row 14
     rowNum = 0
-    for  row in dataframe.iterrows():
+    for index, row in dataframe.iterrows():
         rowData = {
-            "date": get_col_data(dataframe, row "*Date"),
-            "time_start": get_col_data(dataframe, row "*Time Start"),
-            "time_end": get_col_data(dataframe, row "**Time End"),
-            "title": get_col_data(dataframe, row "*Session Title"),
-            "location": get_col_data(dataframe, row "*Room/Location"),
-            "description": get_col_data(dataframe, row "Description"),
-            "speaker": get_col_data(dataframe, row "*Speakers"),
+            "date": get_col_data(dataframe, row, "*Date"),
+            "time_start": get_col_data(dataframe, row, "*Time Start"),
+            "time_end": get_col_data(dataframe, row, "**Time End"),
+            "title": get_col_data(dataframe, row, "*Session Title"),
+            "location": get_col_data(dataframe, row, "*Room/Location"),
+            "description": get_col_data(dataframe, row, "Description"),
+            "speaker": get_col_data(dataframe, row, "Speakers"),
+            "type": get_col_data(dataframe, row, "*Session or Sub-session(Sub)"),
             }
         database.insert(rowData)
         rowNum +=1
